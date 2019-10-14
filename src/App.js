de6c765baf4874ce061d7edff90dc7368c028c53
Collin Smith <cmsmith.tx@gmail.com>
2019-10-14 16:34:29 -0500

    setting up redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+
+const store = createStore(()=> [], {}, applyMiddleware());
 
 function App() {
   return (
-    <div className="App">
-      <PostForm />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
