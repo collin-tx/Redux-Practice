d179b1d4b5ab803baad34ae97e80f27a4073eec2
Collin Smith <cmsmith.tx@gmail.com>
2019-10-14 16:24:34 -0500

    setting up react env

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
 
 export class PostForm extends Component {
+    
+    state = {
+        title: '',
+        body: ''
+    }
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        };
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+            .then(resp => resp.json())
+            .then(data => console.log(data))
+    }
+    
     render() {
         return (
             <div>
-                
+                <h1>Add Post</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Title: </label><br />
+                        <input type='text' name='title' onChange={this.onChange} value={this.state.title} />
+                    </div>
+
+                    <div>
+                        <label>Body: </label><br />
+                        <textarea name='body' value={this.state.body} onChange={this.onChange} />
+                    </div>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
