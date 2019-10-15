9e123398ace84a57ee12ee9d9eba43ac3d562cb4
Collin Smith <cmsmith.tx@gmail.com>
2019-10-15 16:53:28 -0500

    clearing inputs

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -22,6 +22,10 @@ export class PostForm extends Component {
         };
 
         this.props.createPosts(post)
+        this.setState({
+            title: '',
+            body: ''
+        })
     }
     
     render() {
